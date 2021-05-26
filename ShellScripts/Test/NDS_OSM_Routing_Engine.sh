a8174ef3474f8c56043ca6153f1aad82c5f8a071
Vikrant Lanke <lanke@nl1lxl-301994.ttg.global>
2021-05-26 17:34:42 +0530

    script change

--- ShellScripts/Test/NDS_OSM_Routing_Engine.sh
+++ ShellScripts/Test/NDS_OSM_Routing_Engine.sh
@@ -2,16 +2,25 @@
 
 # Global Variables
 GRAPHHOPPER_VERSION=2.3
+# Routing Engine Data directory
 DATA_MOUNT_POINT="/map"
+# Software Installation directory
+SERVER_DIRECTORY="/server"
 
 # Status check for commands
 status_check() {
-  if [ $1 == 0 ]; then
-    echo "$2 successfully!"
+  touch /var/log/NDS_OSM_Routing_Engine.log /var/log/NDS_OSM_Routing_Engine_err.log
+  if [ "$1" == "0" ]; then
+    echo "$2 successfully!" >> /var/log/NDS_OSM_Routing_Engine.log
   else
-    echo "$2 failed!"
+    echo "$2 failed!" >> /var/log/NDS_OSM_Routing_Engine_err.log
     exit 1
   fi
+}
+
+#Replace Line in the File
+replace_line(){
+  sed -i "s@$1.*@$2@g" "$3"
 }
 
 #Install Base Packages
@@ -21,9 +30,9 @@ install_app_packages() {
   status_check $? "Java Packages installation"
 
   #install blobfuse
-  ubuntu_release=`lsb_release -r | awk -F' ' '{print $2}'`
+  ubuntu_release=$(lsb_release -r | awk -F' ' '{print $2}')
   if [ ! -f packages-microsoft-prod.deb ]; then
-    wget https://packages.microsoft.com/config/ubuntu/$ubuntu_release/packages-microsoft-prod.deb
+    wget https://packages.microsoft.com/config/ubuntu/"$ubuntu_release"/packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
   fi
   sudo apt-get update
@@ -35,18 +44,28 @@ install_app_packages() {
   status_check $? "Atomic Lib Packages installation"
 }
 
+#Create Required Directory
+create_directory(){
+  [ ! -d "$SERVER_DIRECTORY" ] && sudo mkdir $SERVER_DIRECTORY
+  sudo chown -R mapmetrics_sa:mapmetrics_sa $SERVER_DIRECTORY
+  sudo chmod -R 777 $SERVER_DIRECTORY
+  [ ! -d $DATA_MOUNT_POINT ] && sudo mkdir $DATA_MOUNT_POINT
+  sudo chown -R mapmetrics_sa:mapmetrics_sa $DATA_MOUNT_POINT
+  sudo chmod -R 777 $DATA_MOUNT_POINT
+}
+
 #Mount the Dataset volume
 mount_data(){
-  [ ! -d "/server/" ] && mkdir /server/
-  touch /server/fuse_connection.cfg
-  echo "accountName $1" >> /server/fuse_connection.cfg
-  echo "authType SPN" >>  /server/fuse_connection.cfg
-  echo "servicePrincipalClientId $2" >> /server/fuse_connection.cfg
-  echo "servicePrincipalTenantId $3" >> /server/fuse_connection.cfg
-  echo "containerName $4" >> /server/fuse_connection.cfg
-  chmod 600 /server/fuse_connection.cfg
-  [ ! -d $DATA_MOUNT_POINT ] && sudo mkdir $DATA_MOUNT_POINT
-  sudo blobfuse $DATA_MOUNT_POINT --tmp-path=/mnt/resource/blobfusetmp  --config-file=/server/fuse_connection.cfg \
+  touch $SERVER_DIRECTORY/fuse_connection.cfg
+  {
+  echo "accountName $1"
+  echo "authType SPN"
+  echo "servicePrincipalClientId $2"
+  echo "servicePrincipalTenantId $3"
+  echo "containerName $4"
+  } >> $SERVER_DIRECTORY/fuse_connection.cfg
+  chmod 600 $SERVER_DIRECTORY/fuse_connection.cfg
+  sudo blobfuse $DATA_MOUNT_POINT --tmp-path=/mnt/resource/blobfusetmp  --config-file=$SERVER_DIRECTORY/fuse_connection.cfg \
        -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120
   status_check $? "Storage Mounting"
 }
@@ -54,53 +73,68 @@ mount_data(){
 download_nds_software(){
   # Download Software from Azure Blob Storage
   temp_mount_point="/mnt/nds_software"
-  [ ! -d "/server/" ] && sudo mkdir /server/
-  sudo touch /server/nds_software.cfg
-  echo "accountName $1" >> /server/nds_software.cfg
-  echo "authType SPN" >> /server/nds_software.cfg
-  echo "servicePrincipalClientId  $2" >> /server/nds_software.cfg
-  echo "servicePrincipalTenantId $3" >> /server/nds_software.cfg
-  echo "containerName $4" >> /server/nds_software.cfg
-  chmod 600 /server/nds_software.cfg
+  sudo touch $SERVER_DIRECTORY/nds_software.cfg
+  {
+    echo "accountName $1"
+    echo "authType SPN"
+    echo "servicePrincipalClientId  $2"
+    echo "servicePrincipalTenantId $3"
+    echo "containerName $4"
+  } >> $SERVER_DIRECTORY/nds_software.cfg
+  chmod 600 $SERVER_DIRECTORY/nds_software.cfg
   sudo mkdir -p $temp_mount_point
-  sudo blobfuse $temp_mount_point --tmp-path=/mnt/resource/nds_software  --config-file=/server/nds_software.cfg \
+  sudo blobfuse $temp_mount_point --tmp-path=/mnt/resource/nds_software  --config-file=$SERVER_DIRECTORY/nds_software.cfg \
        -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120
   status_check $? "NDS Storage Mounting"
-  sudo cp -af $temp_mount_point/* /server/
+  sudo cp -af $temp_mount_point/* $SERVER_DIRECTORY/
+  status_check $? "NDS Data Copy"
   sudo cp -af $temp_mount_point/ConvEngConfig.sqlite /home/mapmetrics_sa/ConvEngConfig.sqlite
-  sudo chown -R mapmetrics_sa:mapmetrics_sa /server/*
-  sudo chmod 777 /server/*
-  rm -rf /server/nds_software.cfg
+  status_check $? "SQLite File Copy"
+  sudo chown -R mapmetrics_sa:mapmetrics_sa $SERVER_DIRECTORY/*
+  sudo chmod 777 $SERVER_DIRECTORY/*
+
+  #clean temporary files
   /bin/umount $temp_mount_point
+  status_check $? "NDS Storage Unmounting"
+  rm -rf $SERVER_DIRECTORY/nds_software.cfg  $temp_mount_point
+  status_check $? "Remove Temporary Data"
 }
 
 start_nds_routing_engine(){
-  /usr/bin/java -jar /server/phonetics-converter-http-server-1.0.4264.jar &
+  /usr/bin/java -jar $SERVER_DIRECTORY/phonetics-converter-http-server-1.0.4264.jar &
   status_check $? "phonetics converter start"
-  /server/NKWorkerEngine serverPort=9090 map=$DATA_MOUNT_POINT phoneticsBaseUrl=http://127.0.0.1:8080 preLoadCache=1 &
+  $SERVER_DIRECTORY/NKWorkerEngine serverPort=9090 map=$DATA_MOUNT_POINT phoneticsBaseUrl=http://127.0.0.1:8080 preLoadCache=1 &
   status_check $? "NKWorkerEngine start"
-  /server/routeservice -listen-addr :6599 -check-interval 15s -max-error-count 120 -endpoint 127.0.0.1:9090 &
+  $SERVER_DIRECTORY/routeservice -listen-addr :6599 -check-interval 15s -max-error-count 120 -endpoint 127.0.0.1:9090 &
   status_check $? "routing service start"
 }
 
 download_osm_software() {
-  [ ! -d "/server/" ] && mkdir /server/
-  sudo chown -R mapmetrics_sa:mapmetrics_sa /server
-  sudo chmod 777 /server
-  #download graph hopper artifact
-  wget -O /server/graphhopper-web.jar https://graphhopper.com/public/releases/graphhopper-web-${GRAPHHOPPER_VERSION}.jar
+  # Log Directory
+  log_directory="$DATA_MOUNT_POINT/graphhopper/logs/$(hostname)"
+  mkdir -p "$log_directory"
+  #download graph hopper artefact
+  wget -O $SERVER_DIRECTORY/graphhopper-web.jar https://graphhopper.com/public/releases/graphhopper-web-${GRAPHHOPPER_VERSION}.jar
   status_check $? "Graphhopper download"
-  #download graph hopper configuration
-  wget -O /server/config.yml https://github.com/tomtom-internal/innovations-map-metrics/blob/master/route-engine/osm/config-${GRAPHHOPPER_VERSION}.yml
+  #download graph hopper config
+  wget -O $SERVER_DIRECTORY/config.yml https://raw.githubusercontent.com/graphhopper/graphhopper/${GRAPHHOPPER_VERSION}/config-example.yml
+
+  #change graph hopper config
+  replace_line "graph.location" "graph.location: /data/graphhopper/202104/graph-cache" $SERVER_DIRECTORY/config.yml
+  replace_line "graph.flag_encoders:" "graph.flag_encoders: car|block_private=false" $SERVER_DIRECTORY/config.yml
+  replace_line "port: 8989" "port: 8080" $SERVER_DIRECTORY/config.yml
+  replace_line "bind_host: localhost" "bind_host: 0.0.0.0" $SERVER_DIRECTORY/config.yml
+  replace_line "port: 8990" "port: 8090" $SERVER_DIRECTORY/config.yml
+  replace_line "current_log_filename:" "current_log_filename: $log_directory/graphhopper.log" $SERVER_DIRECTORY/config.yml
+  replace_line "archived_log_filename_pattern:" "archived_log_filename_pattern: $log_directory/graphhopper-%d.log.gz" $SERVER_DIRECTORY/config.yml
 }
 
 
 start_osm_routing_engine(){
   #extract OSM pbf file path
   # shellcheck disable=SC2006
-  osm_pbf_file_path=`find $DATA_MOUNT_POINT| grep pbf`
-
-  /usr/bin/java -Xmx32G -Ddw.graphhopper.datareader.file="$osm_pbf_file_path" -jar /server/graphhopper-web.jar server /server/config.yml
+  osm_pbf_file_path=`find $DATA_MOUNT_POINT| grep -i pbf`
+  /usr/bin/java -Xmx32G -Ddw.graphhopper.datareader.file="$osm_pbf_file_path" -jar $SERVER_DIRECTORY/graphhopper-web.jar server $SERVER_DIRECTORY/config.yml
   status_check $? "routing service start"
 }
 
@@ -136,23 +170,25 @@ do
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$routingEngineType" ] || [ -z "$spClientID" ] || [ -z "$spTenantID" ] || [ -z "$spClientSecret" ] || [ -z "$accountName" ] || [ -z "$containerName" ] || [ -z "$rtengaccountName" ] || [ -z "$rtengcontainerName" ]
+if [ -z "$routingEngineType" ] || [ -z "$spClientID" ] || [ -z "$spTenantID" ] || [ -z "$spClientSecret" ] || [ -z "$accountName" ] || [ -z "$containerName" ]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
 fi
 
-# Provide Azure SP Secret in file
+# Provide Azure SP Client Secret in file
 sudo /bin/su -c "echo AZURE_STORAGE_SPN_CLIENT_SECRET=$spClientSecret >> /etc/environment"
 
 # Actions based on Routing Engine
 if [[ "$routingEngineType" == "OSM" ]];then
   install_app_packages
+  create_directory
   mount_data "$accountName" "$spClientID" "$spTenantID" "$containerName"
   download_osm_software
   start_osm_routing_engine
 elif [[ "$routingEngineType" == "TTNDS" ]];then
   install_app_packages
+  create_directory
   mount_data "$accountName" "$spClientID" "$spTenantID" "$containerName"
   download_nds_software "$rtengaccountName" "$spClientID" "$spTenantID" "$rtengcontainerName"
   start_nds_routing_engine
