9f562b3bb14e1b39c061a06132eca563692f996a
Vikrant Lanke <lanke@nl1lxl-301994.ttg.global>
2021-05-28 13:30:34 +0530

    script change

--- ShellScripts/Test/print_date.sh
+++ ShellScripts/Test/print_date.sh
@@ -3,10 +3,6 @@
 log_path="/var/log/vikrant-testing.log"
 
 touch $log_path
-echo `date` > $log_path
 echo 'Script Executed' >> $log_path
-echo $1 >> $log_path
-echo $2 >> $log_path
-echo $3 >> $log_path
-echo $4 >> $log_path
+echo `date` > $log_path
 echo '=======================' >> $log_path
